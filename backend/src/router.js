32162aaee463a030a793e9f8812d3fa01b3a3661
PETACCIA <petaccia.seb@gmail.com>
2023-02-02 22:42:52 +0100

    mise en place des routes back pour les pictures

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,9 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const usersControllers = require("./controllers/usersControllers");
+const picturesControllers = require("./controllers/picturesControllers");
 
 router.get("/users", usersControllers.browse);
 router.get("/users/:id", usersControllers.read);
 router.post("/users", usersControllers.add);
 router.put("/users/:id", usersControllers.edit);
+
+router.get("/pictures", picturesControllers.browse);
+router.get("/pictures/:id", picturesControllers.read);
+router.post("/pictures", picturesControllers.add);
+router.put("/pictures/:id", picturesControllers.edit);
+
 module.exports = router;
