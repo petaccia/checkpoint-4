e7e07736c6122634d9c1ee6ead98f54734bee2b4
PETACCIA <petaccia.seb@gmail.com>
2023-02-03 00:33:16 +0100

    mise en place des routes back pour les albums

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const usersControllers = require("./controllers/usersControllers");
 const picturesControllers = require("./controllers/picturesControllers");
+const groupsControllers = require("./controllers/groupsControllers");
+const albumControllers = require("./controllers/albumControllers");
 
 router.get("/users", usersControllers.browse);
 router.get("/users/:id", usersControllers.read);
@@ -15,4 +17,13 @@ router.get("/pictures/:id", picturesControllers.read);
 router.post("/pictures", picturesControllers.add);
 router.put("/pictures/:id", picturesControllers.edit);
 
+router.get("/groups", groupsControllers.browse);
+// router.get("/groups/:id", groupsControllers.read);
+// router.post("/groups", groupsControllers.add);
+// router.put("/pictures/:id", groupsControllers.edit);
+
+router.get("/albums", albumControllers.browse);
+router.get("/albums/:id", albumControllers.read);
+router.post("/albums", albumControllers.add);
+router.put("/albums/:id", albumControllers.edit);
 module.exports = router;
